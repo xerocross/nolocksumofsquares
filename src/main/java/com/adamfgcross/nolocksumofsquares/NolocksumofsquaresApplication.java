efd4750bf7cf7e80408e8fd160d1016f8913575f
Adam <adamfgcross@gmail.com>
2024-10-07 13:46:11 -0400

    initial commit

---
+++ src/main/java/com/adamfgcross/nolocksumofsquares/NolocksumofsquaresApplication.java
@@ -0,0 +1,13 @@
+package com.adamfgcross.nolocksumofsquares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NolocksumofsquaresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NolocksumofsquaresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adamfgcross/nolocksumofsquares/NolocksumofsquaresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adamfgcross.nolocksumofsquares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NolocksumofsquaresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
